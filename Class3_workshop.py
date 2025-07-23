7cc3cd5c2094d02063e821e37050f68fba12110c
showwyx <187162402+showwyx@users.noreply.github.com>
2025-07-23 16:20:06 +0700

    -fix 0.1 workshop class 3

--- Class3_workshop.py
+++ Class3_workshop.py
@@ -1,11 +1,11 @@
 print ("Oh no Monster is attacked!!!")
 print (">>Big Fox (100hp)<<")
-print ("---choose your weapon---")
-print ("1.Pen✎ = 5 dmg")
-print ("2.Cat(ΦωΦ）= 50 dmg")
-print ("3.Tax = 1 dmg (but rich)")
 
 while True:
+    print ("------------------------")
+    print ("1.Pen✎ = 5 dmg")
+    print ("2.Cat(ΦωΦ）= 50 dmg")
+    print ("3.Tax = 1 dmg (but rich)")
     weapon = input("choose your weapon (1,2,3)>>>: ")
 
     if weapon == "1":
@@ -13,7 +13,7 @@ while True:
         break
     elif weapon == "2":
         print ("U choose Cat(ΦωΦ)(50dmg)")
-        
+        break
     elif weapon == "3":
         print ("U choose Tax(1dmg)")
         break
@@ -21,5 +21,13 @@ while True:
         print ("bro just choose 1,2,3")
 
 while True:
-    fight = input("U wanna fight with boss?(1,2): ")
+    fight = input("U wanna fight with boss?(choose 1,2): ")
     if fight == "1":
+        nextfight = input("How many rounds?: ")
+        print ("Let's start!")
+        break
+    elif fight == "2":
+        print ("Exist.")
+        break
+    else:
+        print ("bro just choose 1,2")
