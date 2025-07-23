3ce77b9af0405136b968b3f434f00dc483ddcbd3
showwyx <187162402+showwyx@users.noreply.github.com>
2025-07-23 14:12:09 +0700

    -fix workshop class 3

--- Class3_workshop.py
+++ Class3_workshop.py
@@ -1,31 +1,25 @@
 print ("Oh no Monster is attacked!!!")
-print (">>Big cat (100hp)<<")
-Pen = 1
-Cat = 2
-Tax = 3
+print (">>Big Fox (100hp)<<")
+print ("---choose your weapon---")
+print ("1.Pen✎ = 5 dmg")
+print ("2.Cat(ΦωΦ）= 50 dmg")
+print ("3.Tax = 1 dmg (but rich)")
 
 while True:
-    print ("---choose your weapon---")
-    print ("1. Pen✎ = 5 dmg")
-    print ("2. Cat(ΦωΦ）= 50 dmg")
-    print ("3. Tax = 0 dmg (richB)")
-    Weapon = input("> ")
-    if (Weapon != 1 , 2 , 3):
-        print ("---choose your weapon---")
-        print ("Pen✎ = 5 dmg")
-        print ("Cat(ΦωΦ）= 50 dmg")
-        print ("Tax = 0 dmg (richB)")
-        Weapon = input("> ")
-    else:
-        print (f"U put {Weapon}")
-        print ("u wanna fight Boss?")
-        print ("yes or no")
+    weapon = input("choose your weapon (1,2,3)>>>: ")
+
+    if weapon == "1":
+        print ("U choose Pen✎(5dmg)")
         break
-yes = 0
-no = 0
+    elif weapon == "2":
+        print ("U choose Cat(ΦωΦ)(50dmg)")
+        
+    elif weapon == "3":
+        print ("U choose Tax(1dmg)")
+        break
+    else:
+        print ("bro just choose 1,2,3")
+
 while True:
-    answer = str(input("pick your answer: "))
-    if answer != yes or answer != no:
-        answer = str(input("pick your answer: "))
-    else:
-        break
+    fight = input("U wanna fight with boss?(1,2): ")
+    if fight == "1":
